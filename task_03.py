184b9643127c61640170f8d78c5776aa916684b3
Sergo Bushmakin <giant2788@yahoo.com>
2015-09-20 20:59:50 +0000

    Fixed the file to give no failures.

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Added all the required elements to this file """
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
